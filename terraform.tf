4199731e616d69c4ad39f16c6aef7eed25423d94
Anand_d <anandrl120@gmail.com>
2026-07-13 09:12:07 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,10 +1,3 @@
-
-
-
-
-
-
-
 ############################################################################
 # terraform.tf
 # Main infrastructure definition for Production-Style AWS VPC Architecture
